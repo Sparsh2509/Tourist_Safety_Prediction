4b0fed596387da57d6088f1a8a1002609ab0ab80
Sparsh2509 <sparshgupta250904@gmail.com>
2025-09-22 21:55:37 +0530

    Joblib file created for the api work

--- Tourist_model.py
+++ Tourist_model.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from xgboost import XGBRegressor
+import joblib  # For saving the model
 
 # ================================
 # Load dataset
@@ -24,8 +25,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 # ================================
 # Train XGBoost Regressor with Monotonic Constraints
 # ================================
-# Monotonic constraints: +1 = increasing safety_score as feature increases
-# We assume higher times in red zones, more passes, last_update_gap, deviation -> increase risk
 monotone_constraints = (1, 1, 1, 1, 1)  # order matches X columns
 
 model = XGBRegressor(
@@ -60,5 +59,11 @@ importances = pd.Series(model.feature_importances_, index=X.columns)
 print("\n🔑 Feature Importances (Monotonic):")
 print(importances.sort_values(ascending=False))
 
+# ================================
+# Save the Model for API Use
+# ================================
+
+joblib.dump(model,'Tourist_safety_model.joblib')
+print("Model is saved in joblib file")
 
 
